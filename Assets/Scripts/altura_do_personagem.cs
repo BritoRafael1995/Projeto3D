2891a047f24ebf06798700edad1ba2cab7435a50
Rafael Brito <britorafael1995@gmail.com>
2019-09-30 23:16:14 -0300

    Game 3D plataforma

---
+++ Assets/Scripts/altura_do_personagem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class altura_do_personagem : MonoBehaviour
+{
+    public Transform personagem;
+    Text texto_da_interface;
+    void Start()
+    {
+        texto_da_interface = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        texto_da_interface.text = Mathf.RoundToInt(personagem.position.y).ToString();
+    }
+}
